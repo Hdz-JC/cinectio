c65fa206c2c921d3155f87d53a6f381c86002dad
Hdz-JC <a22150084@itmexicali.edu.mx>
2025-11-14 20:54:16 -0800

    Separacion del list view

--- lib/presentation/screens/movies/home_screen.dart
+++ lib/presentation/screens/movies/home_screen.dart
@@ -1,7 +1,5 @@
-import 'package:cinecito/presentation/providers/movies/movies_providers.dart';
+import 'package:cinecito/presentation/views/home_view.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-
 class HomeScreen extends StatelessWidget {
   static const String routeName = 'home_screen';
   const HomeScreen({super.key});
@@ -9,36 +7,9 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
 
-    return const Scaffold(body: _HomeView());
+    return const Scaffold(body: SafeArea(child: HomeView()));
   }
 }
 
-class _HomeView extends ConsumerStatefulWidget {
-  const _HomeView();
 
-  @override
-  ConsumerState<_HomeView> createState() => _HomeViewState();
-}
 
-class _HomeViewState extends ConsumerState<_HomeView> {
-  @override
-  void initState() {
-    super.initState();
-    ref.read(nowPlayingMoviesProvider.notifier).loadNextPage();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    final nowPlayingMovies = ref.watch(nowPlayingMoviesProvider);
-    if (nowPlayingMovies.isEmpty) {
-      return const Center(child: CircularProgressIndicator());
-    }
-    return ListView.builder(
-      itemBuilder: (_, index) {
-        final movie = nowPlayingMovies[index];
-        return ListTile(title: Text(movie.title));
-      },
-      itemCount: nowPlayingMovies.length,
-    );
-  }
-}
